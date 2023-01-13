5933e2fc021aa0f2dc1dccc432820048deba8cb0
강민지 <68285922+Kminzzi@users.noreply.github.com>
2023-01-13 10:49:30 +0900

    [Bronze V] Title: 꼬마 정민, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+
+        long res = a+b+c;
+
+        System.out.print(res);
+    }
+}
