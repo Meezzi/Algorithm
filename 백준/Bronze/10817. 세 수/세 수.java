73133ee20df12dc8539da6e2a8549f12a283b388
강민지 <68285922+Kminzzi@users.noreply.github.com>
2023-01-15 14:42:28 +0900

    [Bronze III] Title: 세 수, Time: 128 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[3];
+        arr[0] = Integer.parseInt(st.nextToken());
+        arr[1] = Integer.parseInt(st.nextToken());
+        arr[2] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+
+        System.out.print(arr[1]);
+
+    }
+}
