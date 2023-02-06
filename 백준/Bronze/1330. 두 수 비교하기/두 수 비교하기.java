395ecb5ec8b348ead8fa8465aad6320fc671aabb
강민지 <68285922+Meezzi@users.noreply.github.com>
2023-02-06 13:43:19 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        if(a>b) {
+            System.out.print(">");
+        }
+        else if (a<b) {
+            System.out.print("<");
+        }
+        else {
+            System.out.print("==");
+        }
+
+    }
+}
