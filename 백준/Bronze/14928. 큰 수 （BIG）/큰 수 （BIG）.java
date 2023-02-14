d62dfae0355666b85012af22089000704687b394
강민지 <68285922+Meezzi@users.noreply.github.com>
2023-02-14 22:41:19 +0900

    [Bronze V] Title: 큰 수 (BIG), Time: 268 ms, Memory: 20000 KB -BaekjoonHub

---
+++ 백준/Bronze/14928. 큰 수 （BIG）/큰 수 （BIG）.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        long temp = 0;
+        for (int i = 0; i < input.length(); i++) {
+            temp = (temp * 10 + (input.charAt(i) - '0')) % 20000303;
+        }
+
+        System.out.println(temp);
+    }
+}
