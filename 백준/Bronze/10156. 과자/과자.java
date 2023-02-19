c8164a2ecfc50880fe719d7c41c0f1b5493d471e
Meezzi <68285922+Meezzi@users.noreply.github.com>
2023-02-19 20:41:07 +0900

    [Bronze IV] Title: 과자, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        
+        int res = a*b>c ? a*b-c : 0;
+        System.out.println(res);
+    }
+}
