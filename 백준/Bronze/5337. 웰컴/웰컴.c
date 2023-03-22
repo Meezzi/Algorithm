6d930a8497bcde1c445a71613b6441d3b30caaef
Meezzi <alswl-3016@naver.com>
2023-03-22 19:55:01 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    printf(".  .   .\n");
+    printf("|  | _ | _. _ ._ _  _\n");
+    printf("|/\\|(/.|(_.(_)[ | )(/.\n");
+        
+
+    return 0;
+
+}
+
+
+
+
