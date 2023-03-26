3501fc5f987ff10c120901d050520b8dc6d9e12b
Meezzi <alswl-3016@naver.com>
2023-03-26 22:54:42 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 328 ms, Memory: 19612 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -14,41 +14,33 @@ class Main {
     }
 
     public static void stack(String str) {
-        Stack<Integer> stack = new Stack<>();
-        char[] arr = new char[str.length()];
+        Stack<Character> stack = new Stack<>();
 
         for(int i=0; i<str.length(); i++) {
-            arr[i] = str.charAt(i);
-            if(arr[i] == '(') {
-                stack.push(1);
-            } else if (arr[i] == '[') {
-                stack.push(0);
-            }
+            char c = str.charAt(i);
+            if(c == '(' || c == '[') {
+                stack.push(c);
+            } 
+            
+            
+            else if(c==')') {
+                if(stack.empty() || stack.peek()!='(') {
+                    System.out.println("no");
+                    return;
+                } else stack.pop();
 
-            else if((arr[i] == ']' || arr[i] == ')') && stack.empty()) {
-                System.out.println("no");
-                return;
-            }
-            else if (arr[i] == ')' && stack.peek()!=1) {
-                System.out.println("no");
-                return;
-            }
-            else if (arr[i] == ']' && stack.peek()!=0) {
-                System.out.println("no");
-                return;
-            }
-
-            else if(arr[i] == ')' && (stack.peek()==1)) {
-                stack.pop();
-            } else if(arr[i] == ']' && (stack.peek()==0)) {
-                stack.pop();
+            } 
+            
+            else if (c==']') {
+                if(stack.empty() || stack.peek() !='[') {
+                    System.out.println("no");
+                    return;
+                } else stack.pop();
             }
         }
 
         if(stack.empty()) System.out.println("yes");
         else System.out.println("no");
-
-
 
     }
 }
