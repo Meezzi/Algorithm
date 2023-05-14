494b682b44af2b41d446479f5dca30e12cfc6912
Meezzi <alswl-3016@naver.com>
2023-05-14 20:14:04 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 224 ms, Memory: 14940 KB -BaekjoonHub

--- 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -1,19 +1,22 @@
-import java.util.Scanner;
- 
+import java.io.*;
+import java.util.*;
+
 public class Main {
- 
-    public static void main(String[] args) {
- 
-        Scanner sc = new Scanner(System.in);
-        
-        int num1=sc.nextInt(); 
-        
-        for(int i=1; i<=num1; i++) {
-            for(int j=num1; j>=i; j--) {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        int a = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<a; i++) {
+            for(int j=0; j<a-i; j++) {
                 System.out.print("*");
             }
-            System.out.println("");
+            System.out.println();
         }
-        sc.close();
+
+
+
     }
 }
