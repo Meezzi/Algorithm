a9e84439de83790ef000221beeb8fda193b5da05
Meezzi <alswl-3016@naver.com>
2023-05-17 18:30:04 +0900

    [unrated] Title: a와 b 출력하기, Time: 170.70 ms, Memory: 64.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181951. a와 b 출력하기/a와 b 출력하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
