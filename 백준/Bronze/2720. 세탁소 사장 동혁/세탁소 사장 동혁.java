cc9c64bbbb78a1725bfa6368c4a81fba7c639b58
Meezzi <alswl-3016@naver.com>
2023-06-26 20:03:06 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 132 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -3,17 +3,24 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
         for(int i=0; i<n; i++) {
             int a = Integer.parseInt(br.readLine());
 
-            int quter = a/25;
-            int dime = a%25/10;
-            int nickel = a%25%10/5;
-            int penny = a%25%10%5;
+            sb.append(a/25).append(" ");
 
-            System.out.println(quter + " " + dime + " " + nickel + " " + penny);
+            a%=25;
+            sb.append(a/10).append(" ");
+
+            a%=10;
+            sb.append(a/5).append(" ");
+
+            a%=5;
+            sb.append(a).append(" ");
+            sb.append("\n");
         }
+        System.out.println(sb);
     }
 }
