733dd5d99f22745c10c382a9f3c2e6d2f4ae9894
Meezzi <alswl-3016@naver.com>
2023-07-01 22:40:44 +0900

    [Bronze V] Title: A+B - 7, Time: 100 ms, Memory: 12324 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.kt
@@ -0,0 +1,18 @@
+import java.io.*
+import java.util.*
+
+fun main(args: Array<String>) {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+
+    var t = Integer.parseInt(br.readLine())
+
+    for(i in 1 .. t) {
+        val st = StringTokenizer(br.readLine())
+        var n1 = Integer.parseInt(st.nextToken())
+        var n2 = Integer.parseInt(st.nextToken())
+        println(addTwoNumbers(n1, n2, i))
+    }
+}
+
+fun addTwoNumbers(n1: Int, n2: Int, cnt: Int): String = "Case #$cnt: ${n1+n2}"
+
