995d22ee64b38880104c716674f2c473c4ee49f1
Meezzi <alswl-3016@naver.com>
2023-07-01 22:48:29 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 176 ms, Memory: 13504 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.kt
@@ -0,0 +1,18 @@
+import java.io.*
+import java.util.*
+
+fun main(args: Array<String>) {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+
+    var starCnt = Integer.parseInt(br.readLine())
+
+    for(i in 1 .. starCnt) {
+        printStars(i)
+        println()
+    }
+}
+
+fun printStars(star: Int) {
+    for(i in 1.. star) print("*")
+}
+
