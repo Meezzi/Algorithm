edff3d5a6ae0248065c2bdb4515d91b965ae9991
Meezzi <alswl-3016@naver.com>
2023-07-01 18:11:36 +0900

    [Bronze V] Title: 개, Time: 88 ms, Memory: 12024 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.kt
@@ -0,0 +1,15 @@
+import java.io.*
+import java.util.*
+
+fun main(args: Array<String>) {
+    printCat()
+}
+
+fun printCat() {
+    print("""|\_/|
+|q p|   /}
+( 0 )""${'"'}\
+|"^"`    |
+||_/=\\__|""")
+}
+
