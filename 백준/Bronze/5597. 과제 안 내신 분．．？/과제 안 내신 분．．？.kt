2021dde19254cef4e4badcce9fcdd878d16f86b1
Meezzi <alswl-3016@naver.com>
2023-07-05 00:16:44 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 84 ms, Memory: 12080 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.kt
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.kt
@@ -4,14 +4,14 @@ import java.util.*
 fun main(args: Array<String>) {
     val br = BufferedReader(InputStreamReader(System.`in`))
 
-    var intArray = Array<Int>(30) { 0 }
+    var attendance = BooleanArray(30)
 
     for(i in 1 .. 28) {
-        intArray[br.readLine().toInt()-1] = 1
+        attendance[br.readLine().toInt()-1] = true
     }
 
-    for(i in 0 until 30) {
-        if(intArray[i] == 0) println(i+1)
+    for(i in attendance.indices) {
+        if(!attendance[i]) println(i+1)
     }
 
 
