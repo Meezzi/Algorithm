8078f1ca48ca6845fabb3ec6f78d678430e97630
Meezzi <alswl-3016@naver.com>
2023-07-06 23:03:43 +0900

    [Bronze V] Title: 새싹, Time: 88 ms, Memory: 11984 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.kt
@@ -0,0 +1,14 @@
+import java.io.*
+import java.util.*
+
+fun main(args: Array<String>) {
+
+    print("""
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+    """.trimIndent())
+}
