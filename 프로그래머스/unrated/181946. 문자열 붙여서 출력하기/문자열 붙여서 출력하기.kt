11dacfe434375e7aa736296cc5c086d2da287623
Meezzi <alswl-3016@naver.com>
2023-07-11 22:42:08 +0900

    [unrated] Title: 문자열 붙여서 출력하기, Time: 144.65 ms, Memory: 48 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    val input = readln().split(' ')
+    println(solution(input))
+
+}
+
+fun solution(str: List<String>): String {
+    return str.joinToString("")
+}
