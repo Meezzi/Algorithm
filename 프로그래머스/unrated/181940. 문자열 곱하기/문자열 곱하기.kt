e682b374c9ed7f0470e47bc0e5e2ce64669c0235
Meezzi <alswl-3016@naver.com>
2023-07-13 23:20:42 +0900

    [unrated] Title: 문자열 곱하기, Time: 6.70 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.kt
+++ 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.kt
@@ -1,7 +1,5 @@
 class Solution {
     fun solution(my_string: String, k: Int): String {
-        var answer: String = ""
-        for(i in 0 until k) answer += my_string
-        return answer
+        return my_string.repeat(k)
     }
 }
