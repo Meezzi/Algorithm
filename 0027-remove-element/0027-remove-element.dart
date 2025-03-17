cd60b9c18aac2eca13427dd7ef8cbbda32494eff
Meezzi <alswl-3016@naver.com>
2025-03-17 09:41:38 +0900

    Time: 1 ms (32.68%), Space: 150.1 MB (46.37%) - LeetHub

--- 0027-remove-element/0027-remove-element.dart
+++ 0027-remove-element/0027-remove-element.dart
@@ -1,15 +1,21 @@
 class Solution {
+    // int removeElement(List<int> nums, int val) {
+    //     int count = 0;
+    //     // i는 순회 배열
+    //     // count는 저장 위치 포인터
+    //     for (int i = 0; i < nums.length; i++) {
+    //         // val값과 맞지 않으면 저장 위치 포인터에 순회 배열 추가
+    //         if (nums[i] != val) {
+    //             nums[count] = nums[i];
+    //             count++;
+    //         }
+    //     }
+    //     return count;
+    // }
+
     int removeElement(List<int> nums, int val) {
-        int count = 0;
-        // i는 순회 배열
-        // count는 저장 위치 포인터
-        for (int i = 0; i < nums.length; i++) {
-            // val값과 맞지 않으면 저장 위치 포인터에 순회 배열 추가
-            if (nums[i] != val) {
-                nums[count] = nums[i];
-                count++;
-            }
-        }
-        return count;
+        nums.removeWhere((value) => value == val);
+
+        return nums.length;
     }
 }
