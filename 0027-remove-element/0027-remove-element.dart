0a8aaa2fdbd9b42f8c61ac8eeb429297a334dbe4
Meezzi <alswl-3016@naver.com>
2025-03-17 09:39:23 +0900

    Update 0027-remove-element.dart

--- 0027-remove-element/0027-remove-element.dart
+++ 0027-remove-element/0027-remove-element.dart
@@ -1,14 +1,15 @@
 class Solution {
     int removeElement(List<int> nums, int val) {
         int count = 0;
-        List<int> result = [];
+        // i는 순회 배열
+        // count는 저장 위치 포인터
         for (int i = 0; i < nums.length; i++) {
+            // val값과 맞지 않으면 저장 위치 포인터에 순회 배열 추가
             if (nums[i] != val) {
                 nums[count] = nums[i];
                 count++;
             }
         }
-
         return count;
     }
 }
