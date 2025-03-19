9c02db6423779e1e84c95bf0a3fb2c42a5c3b457
Meezzi <alswl-3016@naver.com>
2025-03-19 11:07:31 +0900

    Update 0058-length-of-last-word.dart

--- 0058-length-of-last-word/0058-length-of-last-word.dart
+++ 0058-length-of-last-word/0058-length-of-last-word.dart
@@ -1,15 +1,23 @@
+// class Solution {
+//   int lengthOfLastWord(String s) {
+//     String res = "";
+
+//     for (var i = s.length-1; i >=0; i--) {
+//       if(s[i]== " " && res.isNotEmpty) return res.length; 
+//       if(s[i] == " ") {
+//         continue;
+//       }
+//       res+= s[i];
+//     }
+
+//     return res.length;
+//   }
+// }
+
 class Solution {
   int lengthOfLastWord(String s) {
-    String res = "";
-    
-    for (var i = s.length-1; i >=0; i--) {
-      if(s[i]== " " && res.isNotEmpty) return res.length; 
-      if(s[i] == " ") {
-        continue;
-      }
-      res+= s[i];
-    }
+    var splitString = s.trim().split(' ');
 
-    return res.length;
+    return splitString.last.length;
   }
 }
