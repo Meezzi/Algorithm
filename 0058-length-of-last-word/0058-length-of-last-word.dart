3a52e6ad4ee567412d921446fa7d24c5c26f9f0b
Meezzi <alswl-3016@naver.com>
2025-03-19 11:07:23 +0900

    Time: 0 ms (100%), Space: 148.3 MB (63.04%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.dart
+++ 0058-length-of-last-word/0058-length-of-last-word.dart
@@ -1,6 +1,15 @@
 class Solution {
   int lengthOfLastWord(String s) {
-    var splitString = s.trim().split(' ');
-    return splitString.last.length;
+    String res = "";
+    
+    for (var i = s.length-1; i >=0; i--) {
+      if(s[i]== " " && res.isNotEmpty) return res.length; 
+      if(s[i] == " ") {
+        continue;
+      }
+      res+= s[i];
+    }
+
+    return res.length;
   }
 }
