66cb667e051f69d6f7911f83be0d46c8449c8f77
Meezzi <alswl-3016@naver.com>
2025-03-21 20:10:55 +0900

    Time: 0 ms (100%), Space: 148.4 MB (67.82%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.dart
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.dart
@@ -9,12 +9,19 @@
  */
 class Solution {
   int maxDepth(TreeNode? root) {
+
+    // root가 null이면 종료
     if(root ==  null) {
         return 0;
     }
 
-    int rightDepth = maxDepth(root.right) + 1;
-    int leftDepth = maxDepth(root.left) + 1;
-    return rightDepth > leftDepth ? rightDepth : leftDepth;
+    // 오른쪽 노드의 깊이 구하기
+    int rightDepth = maxDepth(root.right);
+
+    // 왼쪽 노드의 깊이 구하기
+    int leftDepth = maxDepth(root.left);
+
+    // 오른쪽 노드와 왼쪽 노드 중 가장 큰 값을 구한 후, 1을 더해서 반환
+    return 1 + (rightDepth > leftDepth ? rightDepth : leftDepth);
   }
 }
